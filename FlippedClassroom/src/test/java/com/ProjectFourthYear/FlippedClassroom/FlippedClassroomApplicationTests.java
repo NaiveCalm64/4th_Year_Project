fbd56a70d718dd784932feb91c8e69774d9adf49
Agnibha Chakraborty <134518366+NaiveCalm64@users.noreply.github.com>
2024-07-25 14:10:29 +0530

    Initial Commit

---
+++ FlippedClassroom/src/test/java/com/ProjectFourthYear/FlippedClassroom/FlippedClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectFourthYear.FlippedClassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlippedClassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
