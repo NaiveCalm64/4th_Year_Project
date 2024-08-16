217b08a0e76465c790b8a1109e9c3fb07059d299
Agnibha Chakraborty <134518366+NaiveCalm64@users.noreply.github.com>
2024-08-16 23:04:25 +0530

    Create MaterialRepository.java

---
+++ FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/teacher/MaterialRepository.java
@@ -0,0 +1,8 @@
+package com.ProjectFourthYear.FlippedClassroom.teacher;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// import java.lang.String;
+public interface MaterialRepository extends JpaRepository<Material,Long>{
+    
+}
