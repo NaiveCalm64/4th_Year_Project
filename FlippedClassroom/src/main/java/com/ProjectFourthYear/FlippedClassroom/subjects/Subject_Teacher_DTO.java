5af48368642825b57c9b443fc49eae51429c2665
Agnibha Chakraborty <134518366+NaiveCalm64@users.noreply.github.com>
2024-08-16 23:00:44 +0530

    Create Subject_Teacher_DTO.java

---
+++ FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/subjects/Subject_Teacher_DTO.java
@@ -0,0 +1,17 @@
+package com.ProjectFourthYear.FlippedClassroom.subjects;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Subject_Teacher_DTO{
+
+    private String teacher_name;
+    private String subject_name;
+}
