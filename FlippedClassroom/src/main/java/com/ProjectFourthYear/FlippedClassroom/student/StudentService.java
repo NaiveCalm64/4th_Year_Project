0549cf9bea590de5de5e2f0d8973dfa81d893eda
Agnibha Chakraborty <134518366+NaiveCalm64@users.noreply.github.com>
2024-08-16 22:56:48 +0530

    Update StudentService.java ver:1.1

--- FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/student/StudentService.java
+++ FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/student/StudentService.java
@@ -2,8 +2,15 @@ package com.ProjectFourthYear.FlippedClassroom.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ProjectFourthYear.FlippedClassroom.subjects.Subject;
+import com.ProjectFourthYear.FlippedClassroom.subjects.SubjectDTO;
+import com.ProjectFourthYear.FlippedClassroom.subjects.SubjectRepository;
+import com.ProjectFourthYear.FlippedClassroom.subjects.Subject_Student_DTO;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
@@ -11,8 +18,23 @@ public class StudentService {
     @Autowired
     private StudentRepository studentRepository;
 
+    @Autowired
+    private SubjectRepository subjectRepository;
+
     public List<Student> getAllStudents() {
         return studentRepository.findAll();
+    }
+
+    public List<Subject_Student_DTO> getSubjectsByStudentId(String studentId) {
+        List<Object[]> results = subjectRepository.findSubjectsByStudentId(studentId);
+        
+        return results.stream()
+                .map(row -> new Subject_Student_DTO(
+                        ((String) row[0]),              // student ID
+                        (String) row[1]                 // subject id
+                ))
+                .collect(Collectors.toList());
+        // return subjectRepository.findSubjectsByStudentId(studentId);
     }
 
     public Optional<Student> getStudentById(String sid) {
@@ -30,7 +52,7 @@ public class StudentService {
             existingStudent.setName(studentDetails.getName());
             existingStudent.setDepartment(studentDetails.getDepartment());
             existingStudent.setYear(studentDetails.getYear());
-            existingStudent.setSemister(studentDetails.getSemister());
+            existingStudent.setSemester(studentDetails.getSemester());
             existingStudent.setEmail(studentDetails.getEmail());
             existingStudent.setPhone_number(studentDetails.getPhone_number());
             existingStudent.setBirthdate(studentDetails.getBirthdate());
@@ -45,4 +67,3 @@ public class StudentService {
         studentRepository.deleteById(sid);
     }
 }
-
