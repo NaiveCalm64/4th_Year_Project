ac997b04112cd70609fc17a4492810979f62ef49
Agnibha Chakraborty <134518366+NaiveCalm64@users.noreply.github.com>
2024-08-16 22:57:50 +0530

    Update SubjectRepository.java ver:1.1

--- FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/subjects/SubjectRepository.java
+++ FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/subjects/SubjectRepository.java
@@ -1,7 +1,17 @@
 package com.ProjectFourthYear.FlippedClassroom.subjects;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SubjectRepository extends JpaRepository<Subject, String>{
+
+    @Query(value = "SELECT st.name,s.name FROM student_subjects ss inner join subject s on ss.subjects_subid=s.subid inner join student st on ss.student_sid=st.sid  WHERE student_sid = :studentId", nativeQuery = true)
+    List<Object[]> findSubjectsByStudentId(@Param("studentId") String studentId);
+    
+    @Query(value = "SELECT t.name,s.name FROM teacher_subjects ts inner join subject s on ts.subjects_subid=s.subid inner join teacher t on t.tid=ts.teacher_tid  WHERE teacher_tid = :teacherId", nativeQuery = true)
+    List<Object[]> findSubjectsByTeacherId(@Param("teacherId") String teacherId);
 
 }
