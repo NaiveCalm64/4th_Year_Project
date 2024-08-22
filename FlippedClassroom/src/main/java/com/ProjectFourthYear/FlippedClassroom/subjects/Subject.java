2ef00e3b7f0976e47019499a027ea16960887efc
Agnibha Chakraborty <134518366+NaiveCalm64@users.noreply.github.com>
2024-08-22 09:57:11 +0530

    Update Subject.java ver:1.3

--- FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/subjects/Subject.java
+++ FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/subjects/Subject.java
@@ -1,17 +1,18 @@
 package com.ProjectFourthYear.FlippedClassroom.subjects;
 
 import java.util.List;
+
+import com.ProjectFourthYear.FlippedClassroom.teacher.Material;
+
 import java.util.ArrayList;
 
-import com.ProjectFourthYear.FlippedClassroom.student.Student;
-import com.ProjectFourthYear.FlippedClassroom.teacher.Material;
-import com.ProjectFourthYear.FlippedClassroom.teacher.Teacher;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumns;
+import jakarta.persistence.JoinColumn;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,27 +24,17 @@ import lombok.ToString;
 @ToString
 @Entity
 public class Subject {
-	
-	@Id
-	private String subid;
-	private String name;
-	
-	private String department;
-	private int semester;
-	// private String tid;
-	// @ManyToOne
-	// @JoinColumn(name="tid")
-	// private Teacher teacher;
 
-	// @ManyToOne
-	// @JoinColumn(name="sid")
-	// private Student student;
+    @EmbeddedId
+    private Subject_ID subject_id;
 
-	@OneToMany
-	@JoinColumn(name="sub_id")
-	private List<Material> materials=new ArrayList<>();
-	
+    private String name;
+    private int semester;
 
-	
-
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @JoinColumns({
+        @JoinColumn(name = "subject_id", referencedColumnName = "subid"),
+        @JoinColumn(name = "department", referencedColumnName = "department")
+    })
+    private List<Material> materials = new ArrayList<>();
 }
