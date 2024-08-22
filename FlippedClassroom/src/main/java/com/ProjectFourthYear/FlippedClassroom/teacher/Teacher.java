d5d1a7c0298c54b3437276ecc6e023d59c3a7070
Agnibha Chakraborty <134518366+NaiveCalm64@users.noreply.github.com>
2024-08-22 10:03:18 +0530

    Update Teacher.java ver:1.2

--- FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/teacher/Teacher.java
+++ FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/teacher/Teacher.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 import com.ProjectFourthYear.FlippedClassroom.subjects.Subject;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 // import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -25,11 +29,23 @@ public class Teacher {
 	private String tid;
 	private String name;
 	private String email;
+	private String department;
 	private Long phone_number;
 	private LocalDate birthdate;
 	private String password;
 	
-	@OneToMany
+	@ManyToMany(cascade = CascadeType.PERSIST)
+	@JoinTable(
+		name = "teacher_subjects",
+		
+		joinColumns=@JoinColumn(name="teacher_tid"),
+		
+		inverseJoinColumns={
+			@JoinColumn(name="subjects_subid",referencedColumnName = "subid"),
+		 	@JoinColumn(name="department",referencedColumnName = "department")
+		}
+
+	)
 	private List<Subject> subjects=new ArrayList<>();
 	
 	
