1433ddc50259477f2f6c6b515ff0c2c5e34e0a19
Soumyadeep Samanta <soumyadeepsamanta25@gmail.com>
2024-09-12 12:21:37 +0530

    updated 12/09/2024 new

--- FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/teacher/TeacherService.java
+++ FlippedClassroom/src/main/java/com/ProjectFourthYear/FlippedClassroom/teacher/TeacherService.java
@@ -1,16 +1,31 @@
 package com.ProjectFourthYear.FlippedClassroom.teacher;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ProjectFourthYear.FlippedClassroom.subjects.SubjectRepository;
+// import com.ProjectFourthYear.FlippedClassroom.subjects.Subject_Student_DTO;
+import com.ProjectFourthYear.FlippedClassroom.subjects.Subject_Teacher_DTO;
 
 @Service
 public class TeacherService {
 
     @Autowired
     private TeacherRepository teacherRepository;
+    
+    @Autowired
+    private MaterialRepository materialRepository;
+
+    @Autowired 
+    private SubjectRepository subjectRepository;
+
+    // @Autowired
+    // priv
 
     public List<Teacher> getAllTeachers() {
         return teacherRepository.findAll();
@@ -20,8 +35,40 @@ public class TeacherService {
         return teacherRepository.findById(sid);
     }
 
-    public Teacher addTeacher(Teacher Teacher) {
-        return teacherRepository.save(Teacher);
+    public Teacher addTeacher(Teacher teacher) {
+        return teacherRepository.save(teacher);
+    }
+
+    public Material addMaterials(String sub_id,Material material){
+        return materialRepository.save(material);
+    }
+
+    public List<Subject_Teacher_DTO> getSubjectsByTeacherId(String teacherId) {
+        List<Object[]> results = subjectRepository.findSubjectsByTeacherId(teacherId);
+        
+        return results.stream()
+                .map(row -> new Subject_Teacher_DTO(
+                        ((String) row[0]),              // subject id
+                        (String) row[1],                // subject name
+                        (String) row[2],                //department
+                        (int) row[3]                 // semester
+                ))
+                .collect(Collectors.toList());
+        // return subjectRepository.findSubjectsByStudentId(studentId);
+    }
+
+    public List<MaterialDTO> getMaterials(String department,String subid) {
+        List<Object[]> results = materialRepository.findmaterials(department,subid);
+        
+        return results.stream()
+                .map(row -> new MaterialDTO(
+                        (String) row[0],              // subject id
+                        (String) row[1],
+                        (LocalDateTime) row[2]               // subject name
+                    
+                ))
+                .collect(Collectors.toList());
+        // return subjectRepository.findSubjectsByStudentId(studentId);
     }
 
     public Teacher updateTeacher(String tid, Teacher teacherDetails) {
@@ -44,4 +91,8 @@ public class TeacherService {
     public void deleteTeacher(String sid) {
         teacherRepository.deleteById(sid);
     }
+
+    
+
+    
 }
--- frontend/src/components/fc/TeacherHomeComponent.jsx
+++ frontend/src/components/fc/TeacherHomeComponent.jsx
@@ -1,13 +1,59 @@
+import HeaderComponent from "./HeaderComponent"
+import { useState } from "react"
+import { retrieveAllSubjectsForStudentIdApi } from "./api/StudentApiService"
+import { useEffect } from "react"
+import { AuthContext, useAuth } from './security/AuthContext'
+import { BrowserRouter, Routes, Route, useNavigate, useParams, Link } from 'react-router-dom'
+import {retrieveAllSubjectsForTeacherIdApi} from "./api/TeacherApiService"
+import { retrieveAllMaterialsForTeacherIdApi } from "./api/TeacherApiService"
+
 export default function StudentHomeComponent() {
 
-    const subjects = [
-        {id:1, name:"Database Management Systems", department:"CSE" , semister:1},
-        {id:2, name:"Computer Networks", department:"AI-ML" , semister:3},
-        {id:3, name:"Operating Systems", department:"IT" , semister:7}     
-    ]
+    // const subjects = [
+    //     {id:1, name:"Database Management Systems", department:"CSE" , semister:1},
+    //     {id:2, name:"Computer Networks", department:"AI-ML" , semister:3},
+    //     {id:3, name:"Operating Systems", department:"IT" , semister:7}     
+    // ]
+
+    const [subjects, setSubjects] = useState([])
+
+    const [message, setMessage] = useState(null)
+
+    const authContext = useAuth()
+
+    const navigate = useNavigate()
+
+    const username = authContext.username
+
+
+    useEffect ( () => refreshSubjects(), [])
+    
+    function refreshSubjects() {
+        retrieveAllSubjectsForTeacherIdApi(username)
+            .then(response => {
+                console.log(response)
+                setSubjects(response.data)
+            }
+        )
+        .catch(error => console.log(error))
+    }
+
+    function gotoMaterialsPage(subject_id,department) {
+
+                // Fetch materials for the given subject_id and department
+                retrieveAllMaterialsForTeacherIdApi(subject_id, department)
+                .then(response => {
+                    console.log(response)
+                    // Assuming you want to navigate to a new route with the materials data
+                    navigate(`/teacher/${department}/${subject_id}/materials`, { state: { materials: response.data } })
+                })
+                .catch(error => console.log(error))
+    
+    }
 
     return(
         <div className="container">
+            <HeaderComponent></HeaderComponent>
             <h3>Name: </h3>
             <div></div>
             <table className='table'>
@@ -16,7 +62,7 @@ export default function StudentHomeComponent() {
                         <td>Subject Id</td>
                         <td>Subject Name</td>
                         <td>Department</td>
-                        <td>Semister</td>
+                        <td>Semester</td>
                     </tr>
                 </thead>
                 <tbody>
@@ -24,11 +70,11 @@ export default function StudentHomeComponent() {
                         subjects.map(
                             subject=>
                                 <tr>
-                                    <td>{subject.id}</td>
-                                    <td>{subject.name}</td>
+                                    <td>{subject.subject_id}</td>
+                                    <td>{subject.subject_name}</td>
                                     <td>{subject.department}</td>
-                                    <td>{subject.semister}</td>
-                                    <td><button>Enter</button></td>
+                                    <td>{subject.semester}</td>
+                                    <td><button className="btn btn-success" onClick={()=>gotoMaterialsPage(subject.subject_id,subject.department)}>Enter</button></td>
                                 </tr>
                         )
                     }
